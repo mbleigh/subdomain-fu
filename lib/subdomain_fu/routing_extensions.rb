ad30dff03ca01390ba806b041de04b1a2e233914
Jonathon Horsman <jonathon@arctickiwi.com>
2010-01-20 08:48:59 -0700

    Allow host and domain-based routing

--- lib/subdomain_fu/routing_extensions.rb
+++ lib/subdomain_fu/routing_extensions.rb
@@ -13,6 +13,8 @@ module SubdomainFu
       result << "conditions[:subdomain] === env[:subdomain]" if conditions[:subdomain] && conditions[:subdomain] != true && conditions[:subdomain] != false
       result << "SubdomainFu.has_subdomain?(env[:subdomain])" if conditions[:subdomain] == true
       result << "!SubdomainFu.has_subdomain?(env[:subdomain])" if conditions[:subdomain] == false
+      result << "conditions[:domain] === env[:domain]" if conditions[:domain]
+      result << "conditions[:host] === env[:host]" if conditions[:host]
       result
     end
   end
