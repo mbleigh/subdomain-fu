1fe3f3141a3852962cab920a742058611c6e645a
Udaya Kiran <udaykiran.vit@gmail.com>
2015-09-12 13:09:06 +0530

    Fixing the url_for alias method chain as per rails 4.2 's syntax

--- lib/subdomain_fu/url_rewriter.rb
+++ lib/subdomain_fu/url_rewriter.rb
@@ -3,14 +3,14 @@ require 'action_dispatch/routing/route_set'
 module ActionDispatch
   module Routing
     class RouteSet #:nodoc:
-      def url_for_with_subdomains(options, path_segments=nil)
+      def url_for_with_subdomains(options, route_name = nil, url_strategy = UNKNOWN)
         if SubdomainFu.needs_rewrite?(options[:subdomain], (options[:host] || (@request && @request.host_with_port))) || options[:only_path] == false
           options[:only_path] = false if SubdomainFu.override_only_path?
           options[:host] = SubdomainFu.rewrite_host_for_subdomains(options.delete(:subdomain), options[:host] || (@request && @request.host_with_port))
         else
           options.delete(:subdomain)
         end
-        url_for_without_subdomains(options)
+        url_for_without_subdomains(options, route_name, url_strategy)
       end
       alias_method_chain :url_for, :subdomains
     end
